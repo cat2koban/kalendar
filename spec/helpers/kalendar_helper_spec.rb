3a25d42f481c4ca655a173412da596c8c0daf261
Noritomo Taba <cat2koban@users.noreply.github.com>
2020-04-24 16:55:26 +0900

    added empty spec

--- spec/helpers/kalendar_helper_spec.rb
+++ spec/helpers/kalendar_helper_spec.rb
@@ -1,3 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe KalendarHelper do; end
+RSpec.describe KalendarHelper do
+  it do
+    expect(1).to eq(1)
+  end
+end
--- spec/views/kalendar/home.html.erb_spec.rb
+++ spec/views/kalendar/home.html.erb_spec.rb
@@ -1,3 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe 'kalendar/home.html.erb', type: :view do; end
+RSpec.describe 'kalendar/home.html.erb', type: :view do
+  it do
+    expect(1).to eq(1)
+  end
+end
