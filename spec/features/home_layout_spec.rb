796b7bd9fbc9ddebc6dda50662aee4573bd0ab34
Noritomo Taba <cat2koban@users.noreply.github.com>
2020-04-29 12:37:41 +0900

    fixed spec for reviewed

--- spec/features/home_layout_spec.rb
+++ spec/features/home_layout_spec.rb
@@ -3,9 +3,6 @@ require 'rails_helper'
 RSpec.feature 'Home page', type: :feature do
   it do
     visit '/home'
-
-    t = Time.new
-    t = t.strftime('%B')
-    expect(page).to have_text(t)
+    expect(page).to have_text(Time.new.strftime('%B'))
   end
 end
--- spec/requests/kalendar_request_spec.rb
+++ spec/requests/kalendar_request_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Kalendars', type: :request do
   describe 'GET /home' do
     it 'returns http success' do
       get '/home'
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:ok)
     end
   end
 end
