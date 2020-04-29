04df3b968bf041049ed094dd82e4756f292f578d
Noritomo Taba <cat2koban@users.noreply.github.com>
2020-04-29 15:00:39 +0900

    modified only succeed at 200

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "Users", type: :request do
   describe "GET /new" do
     it "returns http success" do
       get "/users/new"
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:ok)
     end
   end
 
