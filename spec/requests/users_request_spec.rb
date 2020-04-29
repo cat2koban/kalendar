79a1650898dd268b4b14320ee1af81f8492e35ee
Noritomo Taba <cat2koban@users.noreply.github.com>
2020-04-29 15:06:23 +0900

    run rubocop -a

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,12 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-
-  describe "GET /signup" do
-    it "returns http success" do
-      get "/signup"
+RSpec.describe 'Users', type: :request do
+  describe 'GET /signup' do
+    it 'returns http success' do
+      get '/signup'
       expect(response).to have_http_status(:ok)
     end
   end
-
 end
