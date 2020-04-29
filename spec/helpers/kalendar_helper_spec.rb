9cc2320a3666feb827e6754a0332f64ba94aa886
Noritomo Taba <cat2koban@users.noreply.github.com>
2020-04-29 11:20:56 +0900

    removed empty specs

--- spec/helpers/kalendar_helper_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe KalendarHelper do
-  it do
-    expect(1).to eq(1)
-  end
-end
--- spec/views/kalendar/home.html.erb_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe 'kalendar/home.html.erb', type: :view do
-  it do
-    expect(1).to eq(1)
-  end
-end
