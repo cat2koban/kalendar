a9bc8a2d7113c8259ea81266e2014e82f7bf2561
Noritomo Taba <cat2koban@users.noreply.github.com>
2020-04-30 12:39:30 +0900

    modified for using random name

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    name { 'Example User' }
+    name { Faker::Name.name }
     sequence(:email) { |n| "user_#{n}@example.com" }
     password { 'password' }
     password_confirmation { 'password' }
