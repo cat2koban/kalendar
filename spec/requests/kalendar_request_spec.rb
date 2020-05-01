26058f3dddeaf91f31a0df8b49a654b5ebcea581
Noritomo Taba <cat2koban@users.noreply.github.com>
2020-05-01 12:03:48 +0900

    fixed path to /home

--- spec/requests/kalendar_request_spec.rb
+++ spec/requests/kalendar_request_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Kalendars', type: :request do
   describe 'GET /home' do
     it 'returns http success' do
-      get '/home'
+      get '/'
       expect(response).to have_http_status(:ok)
     end
   end
