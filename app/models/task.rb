912e60cedbe2403d55bae989cd4e81581b5269fa
Noritomo Taba <cat2koban@users.noreply.github.com>
2020-05-05 14:50:53 +0900

    added validation for easy to see:)

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
+  validates :user_id, presence: true
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :description, length: { maximum: 140 }
 end
