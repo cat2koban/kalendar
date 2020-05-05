48a88b4ad1c4e044f1ec6d496d1d74bec9dce7ea
Noritomo Taba <cat2koban@users.noreply.github.com>
2020-05-05 15:59:31 +0900

    modified because too short

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
-  validates :title, presence: true, length: { maximum: 20 }
-  validates :description, length: { maximum: 140 }
+  validates :title, presence: true, length: { maximum: 40 }
+  validates :description, length: { maximum: 255 }
 end
