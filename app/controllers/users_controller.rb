3bfe979ea667b9e017d39a44e87cc09144464a07
Noritomo Taba <cat2koban@users.noreply.github.com>
2020-05-06 18:01:31 +0900

    removed for not using

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,9 +31,4 @@ class UsersController < ApplicationController
       :password_confirmation
     )
   end
-
-  def correct_user
-    @user = User.find(params[:id])
-    redirect_to root_path unless current_user?(@user)
-  end
 end
