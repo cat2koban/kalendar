1704672de9aa1cccd60e32ee19049682e6cb85cd
Noritomo Taba <cat2koban@users.noreply.github.com>
2020-05-12 14:54:56 +0900

    run db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: '2020_05_07_063707') do
+ActiveRecord::Schema.define(version: 2020_05_12_054756) do
+
+  create_table "groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "members", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "group_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["group_id"], name: "index_members_on_group_id"
+    t.index ["user_id"], name: "index_members_on_user_id"
+  end
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -31,5 +47,7 @@ ActiveRecord::Schema.define(version: '2020_05_07_063707') do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "members", "groups"
+  add_foreign_key "members", "users"
   add_foreign_key "tasks", "users"
 end
